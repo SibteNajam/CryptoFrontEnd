c9ed0915fe9e0e632e0eae878cc004c3672186ce
osmanyousaaf <usmanyousaaaf@gmail.com>
2025-12-10 00:09:33 +0500

    Add Docker deployment config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // Disable to prevent double mounting in development
+  output: 'standalone', // Enable standalone output for Docker
 };
 
 export default nextConfig;
